6eed51d962214ff50166dd257a02bf5f6e3ddb1d
danahAlajmi <danah38x@gmail.com>
2022-06-24 20:00:52 +0300

    DONE

--- App.js
+++ App.js
@@ -1,26 +1,59 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import * as Network from "expo-network";
 
 export default function App() {
+  const [image, setImage] = useState(
+    "https://cdn.sick.com/media/ZOOM/2/82/782/IM0077782.png"
+  );
+  const [pick, setPick] = useState("Pick an image");
+
   const handleOcr = async () => {
-    console.log('handleOcr');
+    let conn = await Network.getNetworkStateAsync();
+    if (!conn.isConnected) {
+      setPick("No Connection.");
+      return;
+    }
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+      setPick("Done!");
+
+      const task = FileSystem.createUploadTask(
+        "http://172.20.10.28000/upload",
+        result.uri,
+        {
+          uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+          fieldName: "photo",
+        },
+        (data) => {
+          console.log({ data });
+        }
+      );
+      let body = task.uploadAsync().then((x) => {
+        console.log({ x });
+      });
+    }
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.card_template}>
-        <Image
-          style={styles.card_image}
-          source={{
-            uri: 'https://cdn.sick.com/media/ZOOM/2/82/782/IM0077782.png',
-          }}
-        />
-        <View style={styles.text_container}>
-          <TouchableOpacity onPress={handleOcr}>
-            <Text style={styles.card_title}>Pick an image</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <Text>Welcome User :D</Text>
+      <Text>{pick}</Text>
+      <Button title="Click here" onPress={handleOcr} />
+      <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -28,13 +61,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   card_template: {
     width: 250,
     height: 250,
-    boxShadow: '10px 10px 17px -12px rgba(0,0,0,0.75)',
+    boxShadow: "10px 10px 17px -12px rgba(0,0,0,0.75)",
   },
   card_image: {
     width: 250,
@@ -42,17 +75,17 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   text_container: {
-    position: 'absolute',
+    position: "absolute",
     width: 250,
     height: 30,
     bottom: 0,
     padding: 5,
-    backgroundColor: 'rgba(0,0,0, 0.3)',
+    backgroundColor: "rgba(0,0,0, 0.3)",
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
   },
   card_title: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
   },
 });
